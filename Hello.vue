b364c6c75bf9e86b84d434d34e26eeb334d3099c
zezhen.zhang <zezhen.zhang@weimob.com>
2020-06-23 16:43:13 +0800

    watch和computed用法

---
+++ Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="root">
+    <div v-for="item in items" :key="item.id">{{ item }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "apple", color: "red" },
+        { name: "bannan", color: "yellow" }
+      ]
+    };
+  },
+  methods: {
+    name() {}
+  },
+  computed: {
+    name() {
+      return this.data;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
